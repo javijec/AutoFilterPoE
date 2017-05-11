8b698c1742162610451cab695f7d1e80b80739b7
Javier <javijec@gmail.com>
2017-05-11 17:52:13 -0300

    Primera Prueba

---
+++ FilterPoE/FilterPoE/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using Newtonsoft.Json;
+
+public class Sparkline
+{
+    public List<double> data { get; set; }
+    public double totalChange { get; set; }
+}
+
+public class ExplicitModifier
+{
+    public string text { get; set; }
+    public bool optional { get; set; }
+}
+
+public class Line
+{
+    public int id { get; set; }
+    public string name { get; set; }
+    public string icon { get; set; }
+    public int mapTier { get; set; }
+    public int levelRequired { get; set; }
+    public string baseType { get; set; }
+    public int stackSize { get; set; }
+    public object variant { get; set; }
+    public object prophecyText { get; set; }
+    public object artFilename { get; set; }
+    public int links { get; set; }
+    public int itemClass { get; set; }
+    public Sparkline sparkline { get; set; }
+    public List<object> implicitModifiers { get; set; }
+    public List<ExplicitModifier> explicitModifiers { get; set; }
+    public string flavourText { get; set; }
+    public string itemType { get; set; }
+    public double chaosValue { get; set; }
+    public double exaltedValue { get; set; }
+    public int count { get; set; }
+}
+
+public class RootObject
+{
+    public List<Line> lines { get; set; }
+}
+
+
+
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (WebClient webClient = new System.Net.WebClient())
+        {
+            WebClient n = new WebClient();
+            var json = n.DownloadString("http://poeninja.azureedge.net/api/Data/GetUniqueMapOverview?league=tmpstandard");
+
+            ///Console.WriteLine(json);
+            RootObject maps = JsonConvert.DeserializeObject<RootObject>(json);
+            for (int i = 0; i < maps.lines.Count; i++)
+            {
+                Console.Write("Nombre del mapa: "); Console.Write(maps.lines[i].name); Console.Write(", Base del mapa: "); Console.Write(maps.lines[i].baseType); Console.Write(", Valor en chaos del mapa: "); Console.WriteLine(maps.lines[i].chaosValue);
+            }
+            Console.ReadKey();
+        }
+    }
+}
