9b5c6f24f0643a3d1c81fd8565e0f0f4da5845f8
Javier Camargo <javijec@gmail.com>
2017-05-14 12:31:03 -0300

    mas cambios

--- FilterPoE/FilterPoE/FilterUniquesMaps.cs
+++ FilterPoE/FilterPoE/FilterUniquesMaps.cs
@@ -10,118 +10,109 @@ public class Sparkline
 }
 
 public class Line
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string Icon { get; set; }
-    public int MapTier { get; set; }
-    public int LevelRequired { get; set; }
-    public string BaseType { get; set; }
-    public int StackSize { get; set; }
-    public object Variant { get; set; }
-    public object ProphecyText { get; set; }
-    public object ArtFilename { get; set; }
-    public int Links { get; set; }
-    public int ItemClass { get; set; }
-    public Sparkline Sparkline { get; set; }
-    public List<object> ImplicitModifiers { get; set; }
-    public List<object> ExplicitModifiers { get; set; }
-    public string FlavourText { get; set; }
-    public string ItemType { get; set; }
-    public double ChaosValue { get; set; }
-    public double ExaltedValue { get; set; }
-    public int Count { get; set; }
-}
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Icon { get; set; }
+        public int MapTier { get; set; }
+        public int LevelRequired { get; set; }
+        public string BaseType { get; set; }
+        public int StackSize { get; set; }
+        public object Variant { get; set; }
+        public object ProphecyText { get; set; }
+        public object ArtFilename { get; set; }
+        public int Links { get; set; }
+        public int ItemClass { get; set; }
+        public Sparkline Sparkline { get; set; }
+        public List<object> ImplicitModifiers { get; set; }
+        public List<object> ExplicitModifiers { get; set; }
+        public string FlavourText { get; set; }
+        public string ItemType { get; set; }
+        public double ChaosValue { get; set; }
+        public double ExaltedValue { get; set; }
+        public int Count { get; set; }
+    }
 
-public class RootObject
+public class Objetos
 {
     public List<Line> lines { get; set; }
 }
-
-
 
 class Program
 {
     static void Main(string[] args)
     {
-        using (WebClient webClient = new System.Net.WebClient())
-        {
-            WebClient n = new WebClient();
-            var json = n.DownloadString("http://poeninja.azureedge.net/api/Data/GetUniqueMapOverview?league=tmpstandard");
-            RootObject UMap = JsonConvert.DeserializeObject<RootObject>(json);
+        WebClient n = new WebClient();
+        var json = n.DownloadString("http://poeninja.azureedge.net/api/Data/GetUniqueMapOverview?league=tmpstandard");
+        Objetos Umaps = new Objetos();
+        Objetos UMap = JsonConvert.DeserializeObject<Objetos>(json);
 
+        //Eleccion de direcctorio y nombre del filtro.
+        var dir = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+        dir = dir.Replace(@"\", @"\\");
+        System.IO.StreamWriter file = new System.IO.StreamWriter(dir + "\\filtro.filter");
 
-            //Eleccion de direcctorio y nombre del filtro.
-            var dir = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
-            dir = dir.Replace(@"\", @"\\");
-            System.IO.StreamWriter file = new System.IO.StreamWriter(dir + "\\filtro.filter");
+        ///rango de valores para seleccion de Mapas
+        Console.Write("Inserte valor minimo de mapas: ");
+        double VmMapa = new double();
+        VmMapa = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Inserte valor T1 de mapas: ");
+        double VT1Mapa = new double();
+        VT1Mapa = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine();
 
-            ///rango de valores para seleccion de Mapas
-            Console.Write("Inserte valor minimo de mapas: ");
-            double VmMapa = new double();
-            VmMapa = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Inserte valor T1 de mapas: ");
-            double VT1Mapa = new double();
-            VT1Mapa = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine();
+        file.WriteLine("#------------------------------------");
+        file.WriteLine("#   [1201] Unique Maps");
+        file.WriteLine("#------------------------------------");
 
-            file.WriteLine("#------------------------------------");
-            file.WriteLine("#   [1201] Unique Maps");
-            file.WriteLine("#------------------------------------");
-
-
-
-            /// Mapas Mayor valor
-            file.WriteLine("Show # Maps:Unique - T1 > " + VT1Mapa + " chaos");
-            file.WriteLine("    Class Maps");
-            file.Write("    BaseType ");
-            for (int i = 0; i < UMap.lines.Count; i++)
+        /// Mapas Mayor valor
+        file.WriteLine("Show # Maps:Unique - T1 > " + VT1Mapa + " chaos");
+        file.WriteLine("    Class Maps");
+        file.Write("    BaseType ");
+        for (int i = 0; i < UMap.lines.Count; i++)
             {
                 if (UMap.lines[i].ChaosValue >= VT1Mapa)
                 {
                     file.Write("\"" + UMap.lines[i].BaseType + "\" ");
                 }
             }
-            file.WriteLine(" ");
-            file.WriteLine("    Rarity Unique");
-            file.WriteLine("    SetFontSize 45");
-            file.WriteLine("");
-            file.WriteLine("    SetTextColor 175 96 37 255           # TEXTCOLOR:	 Uniques");
-            file.WriteLine("    SetBorderColor 175 96 37 255         # BORDERCOLOR:	 Unique Item");
-            file.WriteLine("    SetBackgroundColor 255 255 255 255   # BACKGROUND:	 T1 Global High Value Drop");
-            file.WriteLine("    PlayAlertSound 6 300                 # DROPSOUND:	 T1 Drop");
-            file.WriteLine("");
+        file.WriteLine(" ");
+        file.WriteLine("    Rarity Unique");
+        file.WriteLine("    SetFontSize 45");
+        file.WriteLine("");
+        file.WriteLine("    SetTextColor 175 96 37 255           # TEXTCOLOR:	 Uniques");
+        file.WriteLine("    SetBorderColor 175 96 37 255         # BORDERCOLOR:	 Unique Item");
+        file.WriteLine("    SetBackgroundColor 255 255 255 255   # BACKGROUND:	 T1 Global High Value Drop");
+        file.WriteLine("    PlayAlertSound 6 300                 # DROPSOUND:	 T1 Drop");
+        file.WriteLine("");
 
-            /// Mapas Bajo Valor
-            file.WriteLine("Show # Maps:Unique > " + VmMapa + " chaos");
-            file.WriteLine("    Class Maps");
-            file.Write("    BaseType ");
-            for (int i = 0; i < UMap.lines.Count; i++)
+        /// Mapas Bajo Valor
+        file.WriteLine("Show # Maps:Unique > " + VmMapa + " chaos");
+        file.WriteLine("    Class Maps");
+        file.Write("    BaseType ");
+        for (int i = 0; i < UMap.lines.Count; i++)
+        {
+            if (UMap.lines[i].ChaosValue >= VmMapa && UMap.lines[i].ChaosValue < VT1Mapa)
             {
-                if (UMap.lines[i].ChaosValue >= VmMapa && UMap.lines[i].ChaosValue < VT1Mapa)
-                {
-                    file.Write("\"" + UMap.lines[i].BaseType + "\" ");
-
-                }
+                file.Write("\"" + UMap.lines[i].BaseType + "\" ");
             }
-            file.WriteLine(" ");
-            file.WriteLine("    Rarity Unique");
-            file.WriteLine("    SetFontSize 42");
-            file.WriteLine("");
-            file.WriteLine("    SetBorderColor 175 96 37 255         # BORDERCOLOR:	 Unique Item");
-            file.WriteLine("    PlayAlertSound 4 300                 # DROPSOUND: T1 maps");
-            file.WriteLine("");
-
-            ///Mapas Ocultos
-            file.WriteLine("Hide # Maps:Unique < " + VmMapa + " chaos");
-            file.WriteLine("    Class Maps");
-            file.WriteLine("    Rarity Unique");
-            file.WriteLine("");
-
-
-            file.Close();
-            Console.WriteLine("Creacion del Filtro Terminada");
-            Console.ReadKey();
         }
+        file.WriteLine(" ");
+        file.WriteLine("    Rarity Unique");
+        file.WriteLine("    SetFontSize 42");
+        file.WriteLine("");
+        file.WriteLine("    SetBorderColor 175 96 37 255         # BORDERCOLOR:	 Unique Item");
+        file.WriteLine("    PlayAlertSound 4 300                 # DROPSOUND: T1 maps");
+        file.WriteLine("");
+
+        ///Mapas Ocultos
+        file.WriteLine("Hide # Maps:Unique < " + VmMapa + " chaos");
+        file.WriteLine("    Class Maps");
+        file.WriteLine("    Rarity Unique");
+        file.WriteLine("");
+
+        file.Close();
+        Console.WriteLine("Creacion del Filtro Terminada");
+        Console.ReadKey();
     }
 }
